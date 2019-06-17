3ef7ed2c23eb6b1d2d857952654798b29b3ec9e4
Stephen <rursteve2@yahoo.com>
2019-06-17 04:28:00 -0400

    Updates

---
+++ precache-manifest.23a47a5911d728751f579609f4981c13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e406537383198ed8c16db4e6355bb9e9",
+    "url": "stephencchen.com/index.html"
+  },
+  {
+    "revision": "2707d0ce136906b523f3",
+    "url": "stephencchen.com/static/css/main.3cdcb75d.chunk.css"
+  },
+  {
+    "revision": "596cabd7e9ef0eb61e03",
+    "url": "stephencchen.com/static/js/2.cde98ef6.chunk.js"
+  },
+  {
+    "revision": "2707d0ce136906b523f3",
+    "url": "stephencchen.com/static/js/main.04a0d563.chunk.js"
+  },
+  {
+    "revision": "0f2ff7fc4fcad8088abb",
+    "url": "stephencchen.com/static/js/runtime~main.3f53428f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "stephencchen.com/precache-manifest.23a47a5911d728751f579609f4981c13.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("stephencchen.com/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
