cb83eaee1aa497459ec5f91d96324ac5df0186b4
Stephen <rursteve2@yahoo.com>
2019-06-17 19:31:41 -0400

    edited league api description

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -7,7 +7,7 @@ class Project2 extends Component {
         <div className="project2">
             <div className="projectimgdiv">
                 <img className="projectimg" src="https://i.imgur.com/gpPpFbf.png" alt=""/>
-            <p className="description">Check out the site <a href="https://sei-lol-api.herokuapp.com">here</a></p>
+            <p className="description">Check out the site <a href="https://sei-lol-api.herokuapp.com">here</a>. API key expires every 24 hours. Get an API key <a href="https://developer.riotgames.com/">here</a></p>
             </div>
             <div>
                 <h2>League of Legends Lookup</h2>
